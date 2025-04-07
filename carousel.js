6ee12889d516295540ee81e48f7ab13bf4cc3858
Phyhan4top <oyedepo304@gmail.com>
2025-04-07 12:05:58 +0100

    commit

--- carousel.js
+++ carousel.js
@@ -25,12 +25,32 @@ const cardData = [
     title: "Smart Reader Senior",
     text: "Download our SS Reader and get all your textbooks on  your device.",
   },
+  {
+    icon: "computer.svg",
+    title: "UTME WAEC Past Questions",
+    text: "Solve Past Question from WAEC and UTME  in preparation for your exams.",
+  },
+  {
+    icon: "open-book.svg",
+    title: "IGCSE Master Prep.",
+    text: "Prepare adequately for IGCSE exams .",
+  },
+  {
+    icon: "computer.svg",
+    title: "E-Reader",
+    text: "With Access to our E-Reader make reading easier.",
+  },
+  {
+    icon: "open-book.svg",
+    title: "Smart Reader Senior",
+    text: "Download our SS Reader and get all your textbooks on  your device.",
+  },
 ];
 
 function createCard(icon, title, text) {
   const card = document.createElement("div");
   card.className =
-    "card flex-shrink-0 mx-2 p-4 bg-white text-[#003f7d] rounded-lg text-center transition-all duration-300 scale-90 opacity-50";
+    "card flex-shrink-0 mx-2 p-4 bg-white text-[#003f7d] rounded-lg text-center transition-all duration-300 scale-90 opacity-50 xl:w-1/4 w-full";
   card.innerHTML = `
     <div class="icon-container mx-auto mb-4 bg-[#fd7702] w-24 h-24 rounded-full flex items-center justify-center">
       <img src="./assests/icons/${icon}" alt="" />
@@ -64,7 +84,7 @@ let visibleCards = isMobile ? 1 : 2; // 1 on mobile, 2 on desktop
 // Set initial position
 let position = -index * cardWidth;
 carousel.style.transform = `translateX(${position}px)`;
-
+console.log(carousel.clientWidth / 2);
 // Function to update active cards
 function updateActiveCards() {
   cards.forEach((card, i) => {
